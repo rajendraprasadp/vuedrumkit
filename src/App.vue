47462b3055a6d2ce89e6d7eb2c0ee5aa7d16b4e6
rajendraprasadp <rajuprasadp1@gmail.com>
2022-01-09 21:38:10 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Drumkit</h1>
+    <h5>Press any key below on keyboard or through mouse</h5>
+    <div class="keys">
+      <Key v-for="(key, i) in keys" :sound="key" :key="i" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Key from "./components/Key.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      keys: [
+        {
+          name: "boom",
+          audio: require("./assets/sounds/boom.wav"),
+          letter: "A",
+        },
+        {
+          name: "clap",
+          audio: require("./assets/sounds/clap.wav"),
+          letter: "S",
+        },
+        {
+          name: "hihat",
+          audio: require("./assets/sounds/hihat.wav"),
+          letter: "D",
+        },
+        {
+          name: "kick",
+          audio: require("./assets/sounds/kick.wav"),
+          letter: "F",
+        },
+        {
+          name: "openhat",
+          audio: require("./assets/sounds/openhat.wav"),
+          letter: "G",
+        },
+        {
+          name: "snare",
+          audio: require("./assets/sounds/snare.wav"),
+          letter: "H",
+        },
+        {
+          name: "ride",
+          audio: require("./assets/sounds/ride.wav"),
+          letter: "J",
+        },
+        {
+          name: "tink",
+          audio: require("./assets/sounds/tink.wav"),
+          letter: "K",
+        },
+        {
+          name: "tom",
+          audio: require("./assets/sounds/tom.wav"),
+          letter: "L",
+        },
+      ],
+    };
+  },
+  components: { Key },
+};
 </script>
 
 <style>
@@ -22,5 +75,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.keys {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
